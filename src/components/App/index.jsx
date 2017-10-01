fddb538bef2310f72c5f4996be93b137d61208d5
vanillaSlice <mike_lowe_08@hotmail.co.uk>
2017-10-01 15:12:42 +0100

    Completed Navbar

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -10,9 +10,34 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { showContent: false };
+    this.state = {
+      showContent: false,
+      pressed: [],
+      secretCode: 'ArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRightArrowLeftArrowRightba',
+    };
 
     this.handleTransitionEnd = this.handleTransitionEnd.bind(this);
+    this.handleKeyUp = this.handleKeyUp.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.handleKeyUp, false);
+  }
+
+  handleKeyUp(event) {
+    this.setState((prevState) => {
+      let pressed = [...prevState.pressed, event.key];
+      pressed.splice(
+        -prevState.secretCode.length - 1,
+        pressed.length - prevState.secretCode.length);
+
+      if (pressed.join('').includes(prevState.secretCode)) {
+        window.Cagealicious.add();
+        pressed = [];
+      }
+
+      return { pressed };
+    });
   }
 
   handleTransitionEnd() {
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import FontAwesome from 'react-fontawesome';
 import Transition from 'react-transition-group/Transition';
+import PropTypes from '../../../node_modules/prop-types';
 
-import logo from './logo-md.png';
+import logoMd from './logo-md.png';
+import logoSm from './logo-sm.png';
 
 import './index.css';
 
@@ -23,39 +25,94 @@ class Navbar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = { collapsed: true };
+
+    this.handleTransitionEnd = this.handleTransitionEnd.bind(this);
+    this.handleToggleClick = this.handleToggleClick.bind(this);
+  }
+
+  handleTransitionEnd(node) {
+    node.addEventListener('transitionend', this.props.onComplete);
+  }
+
+  handleToggleClick() {
+    this.setState(prevState => ({ collapsed: !prevState.collapsed }));
   }
 
   render() {
     return (
-      <Transition appear in timeout={0} addEndListener={node => node.addEventListener('transitionend', this.props.onComplete)}>
+      <Transition appear in timeout={0} addEndListener={this.handleTransitionEnd}>
         {state => (
           <nav
-            className="navbar slideInLeft"
+            className={`Navbar ${this.state.collapsed ? 'collapsed' : 'expanded'}`}
             style={{
               ...defaultStyle,
               ...transitionStyles[state],
             }}
           >
-            <img src={logo} alt="Logo" />
-            <h2><span className="first">Mike</span> <span className="second">Lowe</span></h2>
-            <h2><span className="job">Software</span> Developer</h2>
-            <ul>
-              <li><Link to="/">Home</Link><span className="arrow" /></li>
-              <li><Link to="/about">About</Link><span className="arrow" /></li>
-              <li><Link to="/projects">Projects</Link><span className="arrow" /></li>
-              <li><Link to="/skills">Skills</Link><span className="arrow" /></li>
-            </ul>
-            <ul className="social">
-              <li><FontAwesome className="logo" name="envelope" tag="i" /> E-mail</li>
-              <li><FontAwesome className="logo" name="github" tag="i" /> GitHub</li>
-              <li><FontAwesome className="logo" name="codepen" tag="i" /> Codepen</li>
-            </ul>
+            <div className="header">
+              <Link to="/">
+                <img className="logo logo-sm" src={logoSm} alt="Logo" />
+                <img className="logo logo-md" src={logoMd} alt="Logo" />
+              </Link>
+              <h1 className="name">
+                <Link to="/">
+                  <span className="first">Mike</span> <span className="second">Lowe</span>
+                </Link>
+              </h1>
+              <h2 className="job">
+                Software<br /><strong>Developer</strong>
+              </h2>
+              <button
+                type="button"
+                className="toggle"
+                onClick={this.handleToggleClick}
+              >
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar" />
+                <span className="icon-bar" />
+                <span className="icon-bar" />
+              </button>
+            </div>
+            <div className="menu">
+              <div className="links">
+                <NavLink to="/" activeClassName="active" exact>
+                  Home<span className="arrow" />
+                </NavLink>
+                <NavLink to="/about" activeClassName="active">
+                  About<span className="arrow" />
+                </NavLink>
+                <NavLink to="/projects" activeClassName="active">
+                  Projects<span className="arrow" />
+                </NavLink>
+                <NavLink to="/skills" activeClassName="active">
+                  Skills<span className="arrow" />
+                </NavLink>
+              </div>
+              <div className="social">
+                <a href="mailto:mikelowedev@gmail.com">
+                  <FontAwesome className="icon" name="envelope" tag="i" />
+                  <span className="sr-only">E-mail</span>
+                </a>
+                <a href="https://github.com/vanillaSlice">
+                  <FontAwesome className="icon" name="github" tag="i" />
+                  <span className="sr-only">GitHub</span>
+                </a>
+                <a href="https://codepen.io/vanillaSlice/">
+                  <FontAwesome className="icon" name="codepen" tag="i" />
+                  <span className="sr-only">Codepen</span>
+                </a>
+              </div>
+            </div>
           </nav>
         )}
       </Transition>
     );
   }
 }
+
+Navbar.propTypes = {
+  onComplete: PropTypes.func.isRequired,
+};
 
 export default Navbar;
